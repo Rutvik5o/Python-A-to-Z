dedfbbe4c9b275f41f8b8e2557613d739788d0e6
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-09-28 17:13:52 +0530

    Polymorphism

---
+++ P102.py
@@ -0,0 +1,12 @@
+print("Polymorphism->")
+
+print("Duck Typing")
+
+print("Method Overloading")
+
+print("Operator Overloading")
+
+print("Method Overiding")
+
+
+print("Same function having multiple forms")
