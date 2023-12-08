95c65bba4934469403bec5d8329dbd5d63044307
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-08 15:32:42 +0530

    Project-11

---
+++ P107.py
@@ -0,0 +1,4 @@
+print("P107=> BankAccount: Deposit & Withdraw [Project=>11]")
+
+
+print("=> Create A Bank account class having two attributes account holder name & balance & Two Methods:\n1.Deposit\n2.Withdraw\n Make some deposit & withdraw operation but withdrawn amout can not exceed the available balance")
