5d7f8606436b53a6b5232519b5099780bd89b819
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-09 18:46:31 +0530

    Pip & PYPI

---
+++ P108.py
@@ -0,0 +1,7 @@
+print("Packages Collection of Modules")
+
+print("Modules which have a similar type functionlity")
+
+print("We Group these modules together & that is known as package")
+
+print("PYPI=> Python Package Index")
