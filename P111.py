19847de31f9724c0168daf1e210ef656c7691d69
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-10 21:11:45 +0530

    Turtle Graphics

---
+++ P111.py
@@ -0,0 +1,20 @@
+import turtle
+
+
+turtle.getscreen()
+turtle.shape("turtle")
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.lt(90)
+
+turtle.forward(100)
+turtle.lt(90)
+
+turtle.forward(100)
+print(turtle.shape())
+
+turtle.circle(100)
+
+turtle.exitonclick()
