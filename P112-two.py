898eb2e70c57f82555a97781c8154aed1c982623
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-10 21:12:12 +0530

    Turtle Graphis-2

---
+++ P112-two.py
@@ -0,0 +1,13 @@
+from turtle import Turtle,exitonclick
+
+tom=Turtle()
+
+tom.color("red","orange")
+
+tom.begin_fill()
+
+tom.circle(50)
+
+tom.end_fill()
+
+exitonclick()
---
+++ P112.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, exitonclick
+
+tom = Turtle()
+
+tom.shape("turtle")
+tom.color("red", "pink")
+tom.begin_fill()
+tom.circle(100)
+tom.end_fill()
+
+exitonclick()
