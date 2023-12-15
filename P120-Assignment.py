72fcf680f4b30a6dddb028c7434269bde959790a
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-15 21:36:07 +0530

    Py Higher Order Function-> Assignment

---
+++ P120-Assignment.py
@@ -0,0 +1,31 @@
+#Assignment
+
+def add(x,y):
+
+    return x+y
+
+def sub(x,y):
+
+    return x-y
+
+def mul(x,y):
+
+    return x*y
+
+def div(x,y):
+
+    return x/y
+
+def calculator(fun,x,y):
+
+    print(fun(x,y))
+
+
+calculator(add,2,1)
+
+calculator(sub,2,1)
+
+calculator(mul,2,1)
+
+calculator(div,2,1)
+    
